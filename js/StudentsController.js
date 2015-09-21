af2e55f39456d381009414458621d98c97b2e58d
Ashlin Aronin & Deron Johnson <student@epicodus.com>
2015-09-21 10:01:38 -0700

    Add delete functionality for non-BD students

--- js/StudentsController.js
+++ js/StudentsController.js
@@ -1,9 +1,17 @@
 studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
-  $scope.students = [
-    { name: "Ronald Weasley" },
-    { name: "Bob Moses" },
-    { name: "Billbob Reginald Thornsley" },
-    { name: "Schmeal McBeal" },
-    { name: "J" }
-  ]
+  $scope.students = [];
+
+  $scope.addStudent = function() {
+    $scope.students.push({ name: $scope.studentName });
+    $scope.studentName = null;
+  };
+
+  $scope.deleteStudent = function(student) {
+    // You can't delete Brentwood Davis
+    if (student.name !== "Brentwood Davis") {
+      var index = $scope.students.indexOf(student);
+      $scope.students.splice(index, 1);
+    }
+  };
+
 });
