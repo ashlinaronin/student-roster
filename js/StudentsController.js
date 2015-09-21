af49e84fe2586e185b55f8b896593d782363b7a8
Ashlin Aronin & Deron Johnson <student@epicodus.com>
2015-09-21 09:41:49 -0700

    Display student list ;-)

---
+++ js/StudentsController.js
@@ -0,0 +1,9 @@
+studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
+  $scope.students = [
+    { name: "Ronald Weasley" },
+    { name: "Bob Moses" },
+    { name: "Billbob Reginald Thornsley" },
+    { name: "Schmeal McBeal" },
+    { name: "J" }
+  ]
+});
